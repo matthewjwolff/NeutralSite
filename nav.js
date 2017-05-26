33af939473e0c65b18c2af4921c56fff7c5cc8d7
Matthew Wolff <matthewjwolff@gmail.com>
2017-05-26 18:26:24 -0500

    added easter egg

--- nav.js
+++ nav.js
@@ -1,3 +1,19 @@
 $(document).ready(() => {
     $("#nav").load("bar.html")
 })
+
+$(document).on("keypress", (e) => {
+    $(document).off()
+    if(e.key = "f") {
+        $(document).on("keypress", (e) => {
+            $(document).off()
+            if(e.key = "a"){
+                $(document).on("keypress", (e) => {
+                    $(document).off()
+                    if(e.key = "g")
+                        window.location = "fag.mov"
+                })
+            }
+        })
+    }
+})
