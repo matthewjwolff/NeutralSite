4ebb681a939a01d3b4a3d7b4dadbb821976474dd
Matthew Wolff <matthewjwolff@gmail.com>
2017-06-07 15:35:20 -0500

    vue

---
+++ setlist.js
@@ -0,0 +1,10 @@
+//TODO: components
+var vm = new Vue({
+    el: '#songlist',
+    data: {
+        songs: [
+            {title: "jerry", artist: "lewis"},
+            {title: "jerry", artist: "springer"}
+        ]
+    }
+})
