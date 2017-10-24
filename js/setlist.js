49bf1542f1229b6c591736cc873eb260a5499e94
dconfo1 <33045261+dconfo1@users.noreply.github.com>
2017-10-23 21:37:50 -0500

    making sure this works how I think it does

--- js/setlist.js
+++ js/setlist.js
@@ -4,7 +4,10 @@ var vm = new Vue({
     data: {
         songs: [
             {title: "Take On Me", artist: "a-ha"},
-            {title: "Dear Maria, Count Me In", artist: "All Time Low"}
+            {title: "Dear Maria, Count Me In", artist: "All Time Low"},
+            {title: "Dirty Little Secret", artist: "The All-American Rejects"},
+            {title: "Sail", artist: "AWOLNATION"},
+            {title: "Hard to Handle", artist: "The Black Crowes"}
         ]
     }
 })
